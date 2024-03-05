5a5e444f95d36a23923f88ad1429fe7e67ad199c
Jason <jason@Jasons-iMac.lan>
2024-03-05 10:24:44 -0500

    added function get computer choice and playround functions

--- script.js
+++ script.js
@@ -24,3 +24,49 @@ function playRound(playerSelection, computerSelection) {
         return `You lose! ${computerSelection} beats ${playerSelection}.`;
     }
 }
+
+// Function to play the game
+function playGame() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++) {
+        // Prompt user for input
+        let playerSelection = prompt("Enter your choice (Rock, Paper, or Scissors):");
+
+        // Validate user input
+        while (!['rock', 'paper', 'scissors'].includes(playerSelection.toLowerCase())) {
+            playerSelection = prompt("Invalid choice! Please enter Rock, Paper, or Scissors:");
+        }
+
+        // Get computer's choice
+        const computerSelection = getComputerChoice();
+
+        // Play round
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
+
+        // Update scores
+        if (result.includes('win')) {
+            playerScore++;
+        } else if (result.includes('lose')) {
+            computerScore++;
+        }
+    }
+
+    // Determine winner
+    let winner;
+    if (playerScore > computerScore) {
+        winner = "You win!";
+    } else if (playerScore < computerScore) {
+        winner = "You lose!";
+    } else {
+        winner = "It's a tie!";
+    }
+
+    // Print final scores and winner
+    console.log(`Final Scores:\nPlayer: ${playerScore}\nComputer: ${computerScore}\n${winner}`);
+}
+
+// Call the playGame function to start the game
+playGame();
