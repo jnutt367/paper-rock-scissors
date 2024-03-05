c2ff918ff2b8cf3750b8b2631be06b0dba24f836
Jason <jason@Jasons-iMac.lan>
2024-03-05 10:14:04 -0500

    added function get computer choice and playround functions

---
+++ script.js
@@ -0,0 +1,26 @@
+// Function to get the computer's choice
+function getComputerChoice() {
+    const choices = ['Rock', 'Paper', 'Scissors'];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}
+
+// Function to play a single round of Rock Paper Scissors
+function playRound(playerSelection, computerSelection) {
+    // Convert both selections to lowercase for case-insensitive comparison
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    
+    // Determine the winner or tie of the round
+    if (playerSelection === computerSelection) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
+    ) {
+        return `You win! ${playerSelection} beats ${computerSelection}.`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+}
